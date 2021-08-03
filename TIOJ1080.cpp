87423ef61f2e11e5564c28d0a14460497790480a
LJH-coding <61981317+LJH-coding@users.noreply.github.com>
2021-08-03 10:43:54 +0800

    Update TIOJ1080.cpp

--- TIOJ1080.cpp
+++ TIOJ1080.cpp
@@ -3,16 +3,14 @@ using namespace std;
 #define IOS ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 #define int long long
 #define endl '\n'
-#define pb push_back
-const int N = 1e5+5;
+const int N = 1e5+5,inf = 1e9+7;
 int dp[N] = {},ans;
-int a[N];
+int a[N],b[N],l[N],r[N];
 void sol(int v[],int L,int R,int n){
-	if(R-L==1){
+	if(n<=1){
 		return;
 	}
-	int mid = (L+R)/2,l_index = 0,r_index = 0;
-	int l[mid+5],r[mid+5];
+	int mid = (L+R)/2,r_index = 0,l_index = 0;
 	for(int i = 0;i<n;++i){
 		if(v[i]>mid){
 			r[r_index] = v[i];
@@ -28,20 +26,31 @@ void sol(int v[],int L,int R,int n){
 			ans+=dp[i];
 		}
 	}
-	sol(l,L,mid,l_index);
-	sol(r,mid,R,r_index);
+	for(int i = 0;i<l_index;++i) v[i] = l[i];
+	for(int i = 0;i<r_index;++i) v[l_index+i] = r[i];
+	if(l_index == 0 or r_index == 0)return;
+	sol(v,L,*max_element(v,v+l_index),l_index);
+	sol(v+l_index,*min_element(v+l_index,v+n),R,r_index);
 }
 signed main(){
 	IOS;
 	int n,t = 1;
 	while(cin>>n and n){
+		int L = n,R = 1;
 		ans = 0;
 		cout<<"Case #"<<t<<": ";
 		t++;
-		for(int i = 0,j;i<n;++i){
-			cin>>a[i];
+		for(int i = 0;i<n;++i){
+			cin>>a[i],b[i] = a[i];
 		}
-		sol(a,0,n,n);
+		sort(b,b+n);
+		int m = unique(b,b+n)-b;
+		for(int i = 0;i<n;++i){
+			a[i] = lower_bound(b,b+m,a[i])-b+1;
+			if(a[i]<L)L = a[i];
+			if(a[i]>R)R = a[i];
+		}
+		sol(a,L,R,n);
 		cout<<ans<<endl;	
 	}
 }
